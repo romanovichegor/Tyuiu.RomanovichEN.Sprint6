d43145f7656a6461611c991a333d37b362c44f15
Егор Романович <Egor_rom13@mail.ru>
2025-11-27 17:52:54 +0500

    Add project files.

---
+++ Tyuiu.RomanovichEN.Sprint6.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.RomanovichEN.Sprint6.Task0.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tyuiu.RomanovichEN.Sprint6.Task0.V21/Form1.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.RomanovichEN.Sprint6.Task0.V21
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
