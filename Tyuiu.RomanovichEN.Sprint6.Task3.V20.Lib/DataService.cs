3dd8cc272da56ef68548f6a11427456a9fe1b8a9
Егор Романович <Egor_rom13@mail.ru>
2025-12-07 17:14:47 +0500

    Task3

--- Tyuiu.RomanovichEN.Sprint6.Task3.V20.Lib/DataService.cs
+++ Tyuiu.RomanovichEN.Sprint6.Task3.V20.Lib/DataService.cs
@@ -7,14 +7,11 @@ namespace Tyuiu.RomanovichEN.Sprint6.Task3.V20.Lib
         {
             int rows = array.GetUpperBound(0) + 1;
             int columns = array.Length / rows;
-            for (int i = 0; i < rows; i++)
+            for (int j = 0; j < columns; j++)
             {
-                for (int j = 0; j < columns; j++)
+                if (array[5, j] % 2 == 0)
                 {
-                    if (array[5, j] % 2 == 0)
-                    {
-                        array[5, j] = 0;
-                    }
+                    array[5, j] = 0;
                 }
             }
             return array;
