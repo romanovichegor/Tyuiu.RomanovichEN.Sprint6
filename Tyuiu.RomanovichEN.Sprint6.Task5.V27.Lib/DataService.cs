b8831d44a44c4b544b5468b8624d64b4f14f262d
Егор Романович <Egor_rom13@mail.ru>
2025-12-07 19:50:53 +0500

    Task5

---
+++ Tyuiu.RomanovichEN.Sprint6.Task5.V27.Lib/DataService.cs
@@ -0,0 +1,23 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.RomanovichEN.Sprint6.Task5.V27.Lib
+{
+    public class DataService : ISprint6Task5V27
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            double[] res = null;
+            string strx = File.ReadAllText(path);
+            strx = strx.Replace('.', ',');
+            string[] strings = strx.Split(' ');
+            foreach (string s in strings)
+            {
+                double x = Math.Round(double.Parse(s),3);
+                if (x % 5 != 0)
+                {
+                    res.Append(x);
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.RomanovichEN.Sprint6.Task5.V27/FormMain.cs
@@ -0,0 +1,10 @@
+namespace Tyuiu.RomanovichEN.Sprint6.Task5.V27
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+    }
+}
